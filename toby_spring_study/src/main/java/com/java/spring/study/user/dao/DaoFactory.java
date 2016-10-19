edffb7e3a1fcbfd5b2705a95f399e50c9248f0eb
JoJoNaRi <xkgun@naver.com>
2016-10-19 22:21:10 +0900

    1.5 스프링의 IoC
    2016_10_19
    토비의 스프링3.1 : p95 ~ p104

--- toby_spring_study/src/main/java/com/java/spring/study/user/dao/DaoFactory.java
+++ toby_spring_study/src/main/java/com/java/spring/study/user/dao/DaoFactory.java
@@ -5,22 +5,30 @@ package com.java.spring.study.user.dao;
 /**
  * Created by JoJoNaRi on 2016-10-18.
  */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao(){
         UserDao userDao = new UserDao(connectionMaker());
         return userDao;
     }
 
+    @Bean
     public AcountDao acountDao(){
         AcountDao acountDao = new AcountDao(connectionMaker());
         return acountDao;
     }
-
+    @Bean
     public MessageDao messageDao(){
         MessageDao messageDao = new MessageDao(connectionMaker());
         return messageDao;
     }
 
+    @Bean
     public ConnectionMaker connectionMaker(){
         return new DConnectionMaker();
     }
--- toby_spring_study/src/test/java/com/java/spring/sutdy/TestMain.java
+++ toby_spring_study/src/test/java/com/java/spring/sutdy/TestMain.java
@@ -9,28 +9,30 @@ import org.junit.Test;
 
 import com.java.spring.study.user.dao.UserDao;
 import com.java.spring.study.user.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class TestMain {
 	@Test
 	public void main() throws ClassNotFoundException, SQLException {
 
-		UserDao dao = new DaoFactory().userDao();
-		User user = new User();
-		
-		user.setId("1122");
+	    ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);	User user = new User();
+
+		user.setId("11223311");
 		user.setName("백재현");
 		user.setPassword("married");
 		dao.add(user);
-		
+
 		System.out.println(user.getId() + "등록성공");
-		
+
 		User user2 = dao.get(user.getId());
 		System.out.println(user2.getName());
 		System.out.println(user2.getPassword());
-		
+
 		System.out.println(user2.getId() + "조회 성공");
-		
-				
+
+
 
 	}
 
